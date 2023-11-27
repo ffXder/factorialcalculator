720a6b935cb97a5aa37e3b382add4f2571b8b365
ffXder <145956924+ffXder@users.noreply.github.com>
2023-11-27 11:57:16 +0800

    SuggestedName.java

--- SuggestedName.java
+++ SuggestedName.java
@@ -7,7 +7,7 @@ public class SuggestedName {
 		
 		Scanner sc = new Scanner (System.in);
 		
-		//it allows user to input three names
+		//it allows to input three names
 		System.out.println("Enter three first names: ");
 		name1 = sc.nextLine();
 		name2 = sc.nextLine();
@@ -24,37 +24,37 @@ public class SuggestedName {
 		System.out.print("Enter 1-6 to select a name: ");
 		num = sc.nextInt();
 		
-		//if the user enter a number within 1-6 it will display the given result
+		//if the user enter a number within 1-6 it will display 
 		// Using a string method charAt() to find the first character
 		switch(num) {
 		case 1:
-			System.out.println("The initial for " + name1 + " " + name2 + " is: " + name1.charAt(0) + name2.charAt(0));
+			System.out.println("The initial for " + name1 + " " + name2 + " is: " + name1.toUpperCase().charAt(0)+ name2.toUpperCase().charAt(0));
 			System.out.print("Suggested username: " + name1.toLowerCase() + "_" + name2.toLowerCase());
 			break;
 			
 		case 2:
-			System.out.println("The initial for " + name1 + " " + name3 + " is: " + name1.charAt(0) + name3.charAt(0));
+			System.out.println("The initial for " + name1 + " " + name3 + " is: " + name1.toUpperCase().charAt(0) + name3.toUpperCase().charAt(0));
 			System.out.print("Suggested username: " + name1.toLowerCase() + "_" + name3.toLowerCase());
 			break;
 			
 		case 3:
-			System.out.println("The initial for " + name2 + " " + name1 + " is: " + name2.charAt(0) + name1.charAt(0));
+			System.out.println("The initial for " + name2 + " " + name1 + " is: " + name2.toUpperCase().charAt(0) + name1.toUpperCase().charAt(0));
 			System.out.print("Suggested username: " + name2.toLowerCase() + "_" + name1.toLowerCase());
 			break;
 		
 		case 4:
-			System.out.println("The initial for " + name2 + " " + name3 + " is: " + name2.charAt(0) + name3.charAt(0));
+			System.out.println("The initial for " + name2 + " " + name3 + " is: " + name2.toUpperCase().charAt(0) + name3.toUpperCase().charAt(0));
 			System.out.print("Suggested username: " + name2.toLowerCase() + "_" + name3.toLowerCase());
 			break;
 		
 		case 5:
-			System.out.println("The initial for " + name3 + " " + name1 + " is: " + name3.charAt(0) + name1.charAt(0));
+			System.out.println("The initial for " + name3 + " " + name1 + " is: " + name3.toUpperCase().charAt(0) + name1.toUpperCase().charAt(0));
 			System.out.print("Suggested username: " + name3.toLowerCase() + "_" + name1.toLowerCase());
 			break;
 		
 		case 6:
-			System.out.println("The initial for " + name3 + " " + name1 + " is: " + name3.charAt(0) + name1.charAt(0));
-			System.out.print("Suggested username: " + name3.toLowerCase() + "_" + name1.toLowerCase());
+			System.out.println("The initial for " + name3 + " " + name2 + " is: " + name3.toUpperCase().charAt(0) + name2.toUpperCase().charAt(0));
+			System.out.print("Suggested username: " + name3.toLowerCase() + "_" + name2.toLowerCase());
 			break;
 			
 		default:
